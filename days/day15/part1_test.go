083bbe6acca3c09c6519bb4860a58ab3f39cbfc0
Balazs Nadasdi <efertone@pm.me>
2020-12-14 19:15:08 +0100

    Skip tests for days from scaffold

--- days/day15/part1_test.go
+++ days/day15/part1_test.go
@@ -1,32 +1,53 @@
 package day15_test
 
 import (
+	"bytes"
 	"io/ioutil"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/yitsushi/advent-of-code-2020/days/day15"
+	"github.com/yitsushi/advent-of-code-2020/test"
 )
 
 func TestSolver_Part1(t *testing.T) {
+	t.Skip()
+
+	example, err := test.LoadFixture("example")
+	if !assert.NoError(t, err) {
+		return
+	}
+
 	day := day15.Solver{}
 
-	day.SetInput(ioutil.NopCloser(strings.NewReader("")))
+	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
+	if !assert.NoError(t, err) {
+		return
+	}
 
 	out, err := day.Part1()
 
-	assert.Error(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, "", out)
 }
 
-func TestSolver_Part1_noSolution(t *testing.T) {
+func TestSolver_Part1_real(t *testing.T) {
+	t.Skip()
+
+	example, err := test.LoadFixture("input")
+	if !assert.NoError(t, err) {
+		return
+	}
+
 	day := day15.Solver{}
 
-	day.SetInput(ioutil.NopCloser(strings.NewReader("")))
+	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
+	if !assert.NoError(t, err) {
+		return
+	}
 
 	out, err := day.Part1()
 
-	assert.Error(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, "", out)
 }
