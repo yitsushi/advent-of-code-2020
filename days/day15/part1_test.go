035c143a90ee1000eba7ff494074ff2b42a47e03
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2020-12-15 12:05:53 +0100

    Day 15

--- days/day15/part1_test.go
+++ days/day15/part1_test.go
@@ -1,53 +1,38 @@
 package day15_test
 
 import (
-	"bytes"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/yitsushi/advent-of-code-2020/days/day15"
-	"github.com/yitsushi/advent-of-code-2020/test"
 )
 
 func TestSolver_Part1(t *testing.T) {
-	t.Skip()
-
-	example, err := test.LoadFixture("example")
-	if !assert.NoError(t, err) {
-		return
+	testCases := map[string]string{
+		"0,3,6": "436",
+		"1,3,2": "1",
+		"2,1,3": "10",
+		"1,2,3": "27",
+		"2,3,1": "78",
+		"3,2,1": "438",
+		"3,1,2": "1836",
 	}
 
-	day := day15.Solver{}
+	for input, expected := range testCases {
+		day := day15.Solver{}
 
-	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
-	if !assert.NoError(t, err) {
-		return
+		err := day.SetInput(ioutil.NopCloser(strings.NewReader(input)))
+		if !assert.NoError(t, err) {
+			continue
+		}
+
+		out, err := day.Part1()
+
+		assert.NoError(t, err)
+		assert.Equal(t, expected, out)
+
+		break
 	}
-
-	out, err := day.Part1()
-
-	assert.NoError(t, err)
-	assert.Equal(t, "", out)
-}
-
-func TestSolver_Part1_real(t *testing.T) {
-	t.Skip()
-
-	example, err := test.LoadFixture("input")
-	if !assert.NoError(t, err) {
-		return
-	}
-
-	day := day15.Solver{}
-
-	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
-	if !assert.NoError(t, err) {
-		return
-	}
-
-	out, err := day.Part1()
-
-	assert.NoError(t, err)
-	assert.Equal(t, "", out)
 }
--- days/day15/part2_test.go
+++ days/day15/part2_test.go
@@ -1,47 +1,46 @@
 package day15_test
 
 import (
-	"bytes"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/yitsushi/advent-of-code-2020/days/day15"
-	"github.com/yitsushi/advent-of-code-2020/test"
 )
 
 func TestSolver_Part2(t *testing.T) {
-	t.Skip()
-
-	example, err := test.LoadFixture("example")
-	if !assert.NoError(t, err) {
-		return
+	testCases := map[string]string{
+		"0,3,6": "175594",
+		"1,3,2": "2578",
+		"2,1,3": "3544142",
+		"1,2,3": "261214",
+		"2,3,1": "6895259",
+		"3,2,1": "18",
+		"3,1,2": "362",
 	}
 
-	day := day15.Solver{}
+	for input, expected := range testCases {
+		day := day15.Solver{}
 
-	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
-	if !assert.NoError(t, err) {
-		return
+		err := day.SetInput(ioutil.NopCloser(strings.NewReader(input)))
+		if !assert.NoError(t, err) {
+			continue
+		}
+
+		out, err := day.Part2()
+
+		assert.NoError(t, err)
+		assert.Equal(t, expected, out)
+
+		break
 	}
-
-	out, err := day.Part2()
-
-	assert.NoError(t, err)
-	assert.Equal(t, "", out)
 }
 
 func TestSolver_Part2_real(t *testing.T) {
-	t.Skip()
-
-	example, err := test.LoadFixture("input")
-	if !assert.NoError(t, err) {
-		return
-	}
-
 	day := day15.Solver{}
 
-	err = day.SetInput(ioutil.NopCloser(bytes.NewReader(example)))
+	err := day.SetInput(ioutil.NopCloser(strings.NewReader("14,1,17,0,3,20")))
 	if !assert.NoError(t, err) {
 		return
 	}
@@ -49,5 +48,5 @@ func TestSolver_Part2_real(t *testing.T) {
 	out, err := day.Part2()
 
 	assert.NoError(t, err)
-	assert.Equal(t, "", out)
+	assert.Equal(t, "6428", out)
 }
